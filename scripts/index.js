999abb327cad62acbbb649a0e4db35f46b612a11
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-01-08 01:29:08 -0300

    Modificação no codigo HTML e JS (codigo para download do cv)

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,19 @@
-const botaoVermais = document.querySelector('.btn_ampliar');
-const habilidade = document.querySelectorAll('.esconder');
+//DOWNLOAD CV
+
+function downloadCurriculo() {
+    const link = document.createElement('a');
+    link.href = './assets/Currículo - Roberth Vieria.pdf'
+    link.download = 'Curriculo-Roberth.pdf'
+    link.click()
+}
+
 
 
 //BOTÃO SEÇÃO PROJETOS
+
+const botaoVermais = document.querySelector('.btn_ampliar');
+const habilidade = document.querySelectorAll('.esconder');
+
 function clickVer (e) {
 
     let todosVisiveis = true;
