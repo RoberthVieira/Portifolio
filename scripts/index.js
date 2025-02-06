d6c8f1acfd6e04c228696261044c6af6f232e253
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-02-05 23:43:38 -0300

    Adição de funcionalidade de digitação

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const texto = "Estudante de Análise e Desenvolvimento de Sistemas e estou em busca da minha primeira experiência como desenvolvedor Front-end.";
+let i = 0;
+const digitando = document.querySelector('.digitando');
+
+function escrever () {
+    if(i < texto.length) {
+        digitando.innerHTML = texto.substring(0, i + 1) + '<span class="cursor"></span>';
+        i++;
+        setTimeout(escrever, 12);
+    };
+};
+
+escrever();
